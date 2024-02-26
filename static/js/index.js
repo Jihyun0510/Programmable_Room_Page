9aba96fa9cf64f3dbe3c3a5e952d6134ba1a1c63
jhkim0510 <wgus5950@gmail.com>
2024-02-26 22:03:29 +0900

    update

--- static/js/index.js
+++ static/js/index.js
@@ -29,26 +29,16 @@ $(document).ready(function() {
 
     });
 
-    // var options = {
-		// 	slidesToScroll: 1,
-		// 	slidesToShow: 3,
-		// 	loop: true,
-		// 	infinite: true,
-		// 	autoplay: false,
-		// 	autoplaySpeed: 3000,
-    // }
-
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 1,
+			slidesToShow: 3,
 			loop: true,
 			infinite: true,
 			autoplay: false,
 			autoplaySpeed: 3000,
     }
 
-
-		// Initialize all div with carousel class
+  		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
     // Loop on each carousel initialized
